3c1e17011b7e46cddb9ddcda8b123c62b99446ba
DevMoon <shmoon2917@naver.com>
2020-11-29 19:55:28 +0900

    update our menu items to navigate withRouter HOC

---
+++ Lecture/Section 5 - React Router and Routing.md
@@ -0,0 +1,13 @@
+# Section 5 - React Router and Routing
+
+[React Router DOM Docs](https://reactrouter.com/web/guides/quick-start)
+
+## TDIL
+
+### exact attribute
+
+URL이 path 와 정확히 일치해야함을 알려주는 어트리뷰트
+
+### React Router DOM props
+
+리액트 라우터 돔을 통해 렌더되는 컴포넌트는 props 를 받게 되는데, 총 `4가지(history, location, match, staticContext)` 로 구성되어 있다.
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({title, imageUrl, size}) => {
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
+  const linkTo = () => {
+    history.push(`${match.url}${linkUrl}`);
+  };
+
   return (
-    <>
-      <div className={`${size} menu-item`}>
-        <div
-          className="background-image"
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-          }}
-        />
-        <div className="content">
-          <h1 className="title">{title.toUpperCase()}</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
+    <div className={`${size} menu-item`} onClick={linkTo}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
       </div>
-    </>
+    </div>
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
