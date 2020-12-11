3dfcbd206783378d396b6b581e08ce93a1fd9f73
DevMoon <shmoon2917@naver.com>
2020-12-11 16:27:20 +0900

    seperating our cra app into client, added express server to help handle backend's stripe payment

--- Lecture/Todo.md
+++ Lecture/Todo.md
@@ -5,3 +5,4 @@
 3. Firebase Batch Docs 읽기
 4. git 학습
 5. Observable 패턴
+6. useEffect 포스트 읽기
--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,25 +1,41 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     'pk_test_51Hvde7JDUf4u5jXlnqdi7bagZiEQ6Cg0JMfddzUYJdhRk0P7rd361hlJviHprpKGwR4lwk2YXyuTvM6tm4EwBTCl00omuJyplG';
 
-  const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+  const onToken = async (token) => {
+    try {
+      const response = await axios({
+        url: 'payment',
+        method: 'post',
+        data: {
+          amount: priceForStripe,
+          token,
+        },
+      });
+
+      console.log('Payment Successful', response);
+    } catch (error) {
+      console.log('Payment Failed: ', error);
+      alert(
+        'There was an issue with your payment. Please sure you use the provided credit card'
+      );
+    }
   };
 
   return (
     <StripeCheckout
-      label="Pay Now"
-      name="CRWN Clothing"
+      label='Pay Now'
+      name='CRWN Clothing'
       billingAddress
       shippingAddress
       description={`Your total price is $${price}`}
       amount={priceForStripe}
-      panelLabel="Pay Now"
+      panelLabel='Pay Now'
       token={onToken}
       stripeKey={publishableKey}
     />
